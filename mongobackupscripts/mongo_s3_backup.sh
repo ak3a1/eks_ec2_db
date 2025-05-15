89f6583032fb2bdfc6da92a991476f31c514aa51
Akmal Avloni <140357951+ak3a1@users.noreply.github.com>
2025-05-14 17:53:00 -0700

    Update mongo_s3_backup.sh

--- mongobackupscripts/mongo_s3_backup.sh
+++ mongobackupscripts/mongo_s3_backup.sh
@@ -10,8 +10,8 @@
 
 MONGO_BACKUP_FILENAME=$(date "+%H-%M-%S-UTC.mongo_backup" -u)
 
-mongodump -h localhost:27017 -d my_mongo_db_name -o $MONGO_BACKUP_FILENAME
+mongodump -h localhost:27017 -d mydb -o $MONGO_BACKUP_FILENAME
 
-aws s3 cp $MONGO_BACKUP_FILENAME s3://my-tf-mongo-backup-bucket-for-wiz-testing-environment-akmal/$(date +%m-%d-%y)/
+aws s3 cp $MONGO_BACKUP_FILENAME s3://my-tf-mongo-backup-bucket-for-wiz-testing-environment-akmal/$(date +%m-%d-%y) --recursive
 
-rm $MONGO_BACKUP_FILENAME
+rm -rf $MONGO_BACKUP_FILENAME
